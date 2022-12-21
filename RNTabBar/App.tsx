5750323f194500ef2cfbdd3e6f24da143f5cb997
Hasith Methmal Jayasekara <hasith.m@eyepax.com>
2022-12-21 11:10:07 +0530

    [chore] project init

---
+++ RNTabBar/App.tsx
@@ -0,0 +1,14 @@
+import React, {type PropsWithChildren} from 'react';
+import {StatusBar, StyleSheet, Text, useColorScheme, View} from 'react-native';
+
+const App = () => {
+  return (
+    <>
+      <StatusBar barStyle={'light-content'} />
+    </>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default App;
